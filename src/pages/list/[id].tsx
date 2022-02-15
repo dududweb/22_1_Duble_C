f2bcc17c852c9c182a803847b62248f4c993034f
Moro-yong <g76eho@gmail.com>
2022-02-15 22:05:58 +0900

    Fix: 충돌해결

--- src/pages/list/[id].tsx
+++ src/pages/list/[id].tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import styles from 'styles/components/store/store.module.scss';
 import Storeheader from '../../components/store/storeheader/Index';
 import ProductCard from '../../components/store/productCard/index';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 function ProductList() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [itemData, setItemData] = useState({});
+
+  useEffect(() => {
+    axios(`https://api2.ncnc.app/con-category1s/${Id}/nested`).then(res =>
+      setItemData(res),
+    );
+  }, []);
   return (
     <div>
       <Storeheader />
