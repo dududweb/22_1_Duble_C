9d1e6a39421af5e18e72fd96742e41fa762e7133
JooyoungHwnag <joodudsdx@gmail.com>
2022-03-10 20:56:47 +0900

    Fix: nav 주석 삭제 배포에러

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -6,12 +6,6 @@ function Nav() {
   return (
     <nav className={styles.nav}>
       <StoreHeader />
-      {/* <div className={styles.navWrap}>
-        <button className={styles.menu}>
-          <img src="/images/icon/menu.png" />
-        </button>
-        <h1 className={styles.logo}>니콘내콘</h1>
-      </div> */}
     </nav>
   );
 }
