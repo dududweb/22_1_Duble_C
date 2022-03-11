4d09091d026e2120d18c4fedf8797d48a83c34a4
JooyoungHwnag <joodudsdx@gmail.com>
2022-03-11 12:34:30 +0900

    Fix: 절대경로 폴더 수정

--- src/pages/brands/[id].tsx
+++ src/pages/brands/[id].tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from 'styles/components/store/storeGridLayout.module.scss';
 import ProductsCardList from 'components/ProductCardList';
-import StoreHeader from 'components/StoreHeader';
+import StoreHeader from 'components/StoreHeader/index';
 import { useRouter } from 'next/router';
 import { API } from 'constants/api';
 import { path } from 'constants/path';
