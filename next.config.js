9053d7b181bd250a31725381d222d88182ac18a5
JooyoungHwnag <joodudsdx@gmail.com>
2022-03-11 11:01:56 +0900

    Fix: next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  ignoreDuringBuilds: true,
 };
 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-};
+module.exports = nextConfig;
