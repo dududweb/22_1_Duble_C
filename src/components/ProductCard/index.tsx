64dfa74d540020e224f3ad426b81dc8bbaae444c
JooyoungHwnag <joodudsdx@gmail.com>
2022-03-11 20:27:43 +0900

    Fix: 상세페이지 상품 클릭 막기 위해 link 리스트로 이동

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import styles from './styles.module.scss';
 import { ConItem } from 'types/productOfBrands';
 
@@ -17,25 +16,22 @@ function ProductCard({
   warning,
   isOnlyAccount,
   imageUrl,
-  path,
 }: ConItem) {
   console.log('productList.id>>>>', id);
   return (
-    <Link href={`${path}${id}`}>
-      <a className={styles.itemContents}>
-        <div className={styles.itemImage}>
-          <img src={imageUrl} alt="img" />
+    <div className={styles.itemContents}>
+      <div className={styles.itemImage}>
+        <img src={imageUrl} alt="img" />
+      </div>
+      <div className={styles.itemText}>
+        <div className={styles.productName}>{name}</div>
+        <div>
+          <span className={styles.sales}>{discountRate}%</span>
+          <span className={styles.price}>{minSellingPrice}원</span>
+          <span className={styles.disablePrice}>{originalPrice}원</span>
         </div>
-        <div className={styles.itemText}>
-          <div className={styles.productName}>{name}</div>
-          <div>
-            <span className={styles.sales}>{discountRate}%</span>
-            <span className={styles.price}>{minSellingPrice}원</span>
-            <span className={styles.disablePrice}>{originalPrice}원</span>
-          </div>
-        </div>
-      </a>
-    </Link>
+      </div>
+    </div>
   );
 }
 
