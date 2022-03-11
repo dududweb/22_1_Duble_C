fe4f65a1de207619bb81ba100e494dec0074d3f2
JooyoungHwnag <joodudsdx@gmail.com>
2022-03-11 12:11:20 +0900

    Fix: next.config.js 수정 배포에러수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const path = require('path');
+
+const nextConfig = phase => {
+  webpack(config, { webpack });
+  config.resolve = {
+    alias: {
+      pages: path.resolve(__dirname, 'src/pages'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      components: path.resolve(__dirname, 'src/components'),
+      styles: path.resolve(__dirname, 'styles'),
+      icon: path.resolve(__dirname, 'public/icon'),
+      types: path.resolve(__dirname, 'src/types'),
+      constants: path.resolve(__dirname, 'src/constants'),
+    },
+    ...config.resolve,
+  };
+  return config;
 };
 
-module.exports = nextConfig;
+module.exports = phase => nextConfig(phase);
