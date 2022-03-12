8d2d6b076f980c90a41dcb5c7afa07a7c328d638
JooyoungHwnag <joodudsdx@gmail.com>
2022-03-12 20:11:54 +0900

    Fix: 해더레이아웃 패딩값 수정

--- src/components/NavOfCategories/index.tsx
+++ src/components/NavOfCategories/index.tsx
@@ -3,7 +3,6 @@ import styles from './styles.module.scss';
 import { ConCategory } from 'types/interface';
 import Link from 'next/link';
 import { path } from 'constants/path';
-import { useRouter } from 'next/router';
 
 interface NavOfCategories {
   categoryLists: ConCategory[];
@@ -11,28 +10,28 @@ interface NavOfCategories {
 }
 
 function NavOfCategories({ categoryLists, urlId }: NavOfCategories) {
-  const router = useRouter();
-
   return (
     <section className={styles.navBox}>
-      <ul className={styles.navList}>
-        {categoryLists.map(list => {
-          return (
-            <li
-              className={
-                list.id == urlId
-                  ? `${styles.list} ${styles.selectedMenuRed}`
-                  : styles.list
-              }
-              key={list.id}
-            >
-              <Link href={`${path.categories}${list.id}`}>
-                <a>{list.name}</a>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <div className={styles.navList}>
+        <ul className={styles.innerList}>
+          {categoryLists.map(list => {
+            return (
+              <li
+                className={
+                  list.id == urlId
+                    ? `${styles.list} ${styles.selectedMenuRed}`
+                    : styles.list
+                }
+                key={list.id}
+              >
+                <Link href={`${path.categories}${list.id}`}>
+                  <a>{list.name}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </section>
   );
 }
--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -8,14 +8,16 @@ interface PageHeaderProps {
 function PageHeader({ title }: PageHeaderProps) {
   return (
     <header className={styles.header}>
-      <div className={styles.leftBox}>
-        <button className={styles.backButton}>
-          <img src="/images/icon/Back.png" alt="뒤로가기 버튼" />
-        </button>
-      </div>
-      <h2 className={styles.title}>{title ? title : '니콘내콘'}</h2>
-      <div className={styles.rightBox}>
-        <button className={styles.rightButton}></button>
+      <div className={styles.innerHeader}>
+        <div className={styles.leftBox}>
+          <button className={styles.backButton}>
+            <img src="/images/icon/Back.png" alt="뒤로가기 버튼" />
+          </button>
+        </div>
+        <h2 className={styles.title}>{title ? title : '니콘내콘'}</h2>
+        <div className={styles.rightBox}>
+          <button className={styles.rightButton}></button>
+        </div>
       </div>
     </header>
   );
