15445c9f313c29579c9895abd47b3885dec90b51
JooyoungHwnag <joodudsdx@gmail.com>
2022-03-12 21:09:28 +0900

    Add: 네브 헤더 뒤로가기 기능 추가 홈 메뉴바로 조건부 렌더링

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 
 interface PageHeaderProps {
   title?: string;
 }
 
 function PageHeader({ title }: PageHeaderProps) {
+  const router = useRouter();
+  const url = router.asPath;
+  console.log(url);
   return (
     <header className={styles.header}>
       <div className={styles.innerHeader}>
         <div className={styles.leftBox}>
-          <button className={styles.backButton} onClick={() => Router.back()}>
-            <img src="/images/icon/Back.png" alt="뒤로가기 버튼" />
-          </button>
+          {url === '/' ? (
+            <button className={styles.menuButton}>
+              <img src="/images/icon/menu.png" alt="홈 버튼" />
+            </button>
+          ) : (
+            <button className={styles.backButton} onClick={() => Router.back()}>
+              <img src="/images/icon/Back.png" alt="뒤로가기 버튼" />
+            </button>
+          )}
         </div>
         <h2 className={styles.title}>{title ? title : '니콘내콘'}</h2>
         <div className={styles.rightBox}>
