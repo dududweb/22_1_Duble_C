fbe1b9b5244c13eb677aa75bccf1103766a8970c
JooyoungHwnag <joodudsdx@gmail.com>
2022-03-13 23:06:46 +0900

    Add: 고객센터 구매,판매 버튼 레이아웃완료

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -4,26 +4,44 @@ import { API } from 'constants/api';
 import { path } from 'constants/path';
 import axios from 'axios';
 import PageHeader from 'components/PageHeader';
+import styles from './styles.module.scss';
+import { FaqType } from 'types/faq';
 
 export const getServerSideProps: GetServerSideProps = async (context: any) => {
   const { id } = context.query;
   const { data } = await axios('https://api2.ncnc.app/qa-types');
 
-  const categoryData = data.qaTypes;
+  const qaTypes = data.qaTypes;
 
   return {
     props: {
-      categoryData,
+      qaTypes,
     },
   };
 };
 
-function Contacts({ categoryData }: any) {
-  console.log(categoryData);
+interface ContactsProps {
+  qaTypes: FaqType[];
+}
+
+function Contacts({ qaTypes }: ContactsProps) {
+  console.log(qaTypes);
   return (
-    <div>
+    <div className={styles.contacts}>
       <PageHeader title="고객센터" />
-      <div>test</div>
+      <div className={styles.timeNotice}>
+        <h3>상담시간 안내</h3>
+        <p>평일 10:00 - 18:00</p>
+        <p>점심시간 12:30 - 13:30 / 토 ・ 일 ・ 공휴일 휴무</p>
+      </div>
+      <div className={styles.faqBox}>
+        <h3>자주 묻는 질문</h3>
+        <div className={styles.faqTypeBox}>
+          {qaTypes.map(type => {
+            return <button className={styles.typeButton}>{type.name}</button>;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/types/faq.ts
@@ -0,0 +1,5 @@
+export interface FaqType {
+  id: number;
+  key: string;
+  name: string;
+}
